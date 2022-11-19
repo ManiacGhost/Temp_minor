3cad03f4ae84be3c44eef79b6e6c0e1af7db5a39
Harsh Pandey <harshpandey6754@gmail.com>
2022-11-19 14:15:03 +0530

    newlyadded
    newly added files

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,9 +8,9 @@ class Nav extends Component {
               <li className="logo">Jaypee Institute of Infomation Technology</li>
           </ul>
           <ul>
-              <li><a href="contact">Form</a></li>
+              <li><a href="#contact">Form</a></li>
               <li><a href="https://webportal.jiit.ac.in:6010/employeeportal/">Webportal</a></li>
-              <li><a href="#">Creators</a></li>
+              <li><a href="creators.html">Creators</a></li>
           </ul>
       </nav>
     );
