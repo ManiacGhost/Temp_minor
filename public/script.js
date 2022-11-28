cf56ca8c05c234ea528dbe2def13d5e0a09881b3
legacy243mudit <109586517+legacy243mudit@users.noreply.github.com>
2022-11-28 21:22:20 +0530

    ff
    fff

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
-function generatepdf()
-{
-    const element = document.getElementById("invoice");
+window.onload=function(){
+    document.getElementById("download")
+    .addEventListener("click",()=>{
+        const invoice = this.document.getElementById("invoice");
+        console.log(invoice);
+        console.log(window);
+        var opt = {
+            margin: 1,
+            filename: 'myfile.pdf',
+            image: { type: 'jpeg', quality:0.98},
+            html2canvas: { scale:5},
+            jsPDF: { unit: 'mm', format: 'a2', orientation: 'landscape' }
+        
 
-    html2pdf()
-    .from(element)
-    .save()
+        }
+        html2pdf().from(invoice).set(opt).save();
+    })
 }
