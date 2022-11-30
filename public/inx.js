144dbf909ee88025370489da6ee521cbecefc106
legacy243mudit <109586517+legacy243mudit@users.noreply.github.com>
2022-11-30 23:56:10 +0530

    bb
    boob

---
+++ public/inx.js
@@ -0,0 +1,28 @@
+const { PDFDocument } = require('pdf-lib');
+const { readFile, writeFile } = require('fs/promises');
+
+async function createPdf(input, output)
+{
+    try{
+        const pdfDoc = await PDFDocument.load(await readFile(input));
+
+        const fieldnames = pdfDoc.getForm().getFields().map(f => f.getName());
+        console.log({ fieldnames });
+
+        const form = pdfDoc.getForm();
+
+        form.getTextField('Text-viyCwNmf93').setText('Mudi');
+        form.getTextField('Text-uZW5I-RnXc').setText('Assistant teacher');
+        
+        
+        const pdfBytes = await pdfDoc.save();
+
+        await writeFile(output, pdfBytes);
+        console.log('PDF created');
+    }
+    catch(err){
+        console.log(err);
+        }
+    
+}
+    createPdf('appraisal.pdf','output.pdf');
